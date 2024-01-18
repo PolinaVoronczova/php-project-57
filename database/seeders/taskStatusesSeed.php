db95edeee473881d5a9600636371ebc0dee7fa5d
VoronczovaPolina <tirabyrius@gmail.com>
2024-01-18 18:42:52 +0500

    create seed

---
+++ database/seeders/taskStatusesSeed.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class taskStatusesSeed extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
