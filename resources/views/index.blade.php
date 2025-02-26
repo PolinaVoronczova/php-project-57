2e6a25cfffccb73671898eb3e1fd3ef88570f725
PolinaVoronczova <tirabyrius@gmail.com>
2025-02-26 19:53:05 +0500

    add homepage and app.layout

---
+++ resources/views/index.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.app')
+
+@section('title', 'Менеджер задач')
+
+@section('content')
+<div class="mr-auto place-self-center lg:col-span-7">
+    <h1 class="max-w-2xl mb-4 text-4xl font-extrabold leading-none tracking-tight md:text-5xl xl:text-6xl dark:text-white">
+        Привет!
+    </h1>
+    <p class="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">
+        Это простой менеджер задач на Laravel
+    </p>
+
+</div>
+@endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,22 +15,50 @@
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100">
-            @include('layouts.navigation')
-
             <!-- Page Heading -->
-            @isset($header)
-                <header class="bg-white shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                        {{ $header }}
+            <header class="fixed w-full">
+                <nav class="bg-white border-gray-200 py-2.5 dark:bg-gray-900 shadow-md">
+                    <div class="flex flex-wrap items-center justify-between max-w-screen-xl px-4 mx-auto">
+                        <a href="/" class="flex items-center">
+                            <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Менеджер задач</span>
+                        </a>
+                        <div class="flex items-center lg:order-2">
+                            <a href=" route('login') " class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                                Вход
+                            </a>
+                            <a href=" route('register') " class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-2">
+                                Регистрация
+                            </a>
+                        </div>
+
+                        <div class="items-center justify-between hidden w-full lg:flex lg:w-auto lg:order-1">
+                            <ul class="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
+                                <li>
+                                    <a href=" route('tasks') " class="block py-2 pl-3 pr-4 text-gray-700 hover:text-blue-700 lg:p-0">
+                                        Задачи
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href=" route('task_statuses') " class="block py-2 pl-3 pr-4 text-gray-700 hover:text-blue-700 lg:p-0">
+                                        Статусы
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="route('labels')" class="block py-2 pl-3 pr-4 text-gray-700 hover:text-blue-700 lg:p-0">
+                                        Метки
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
                     </div>
-                </header>
-            @endisset
+                </nav>
+            </header>
 
             <!-- Page Content -->
-            <main>
-                {{ $slot }}
-            </main>
-        </div>
+            <section class="bg-white dark:bg-gray-900">
+                <div class="grid max-w-screen-xl px-4 pt-20 pb-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12 lg:pt-28">
+                    @yield('content')
+                </div>
+            </section>
     </body>
 </html>
